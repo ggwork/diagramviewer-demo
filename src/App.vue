3ecca54ca9a19497b468decb3c805d66c967ad6a
郭宪强 <guoxq@wanhuhealth.com>
2022-09-06 17:23:00 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="main">
+    <div class="container" id="container"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// @ts-ignore
+// import DiagramViewer from "@ggwork/diagramviewer";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  let data_url = "http://localhost:1337/api/diagrams/58";
+  let container = document.querySelector("#container");
+  // @ts-ignore
+  new window.DiagramViewer(container, data_url);
+});
+</script>
+<style>
+.main {
+  width: 100vw;
+  height: 100vh;
+}
+.container {
+  width: 500px;
+  height: 300px;
+  border: 1px solid #eee;
+  margin: auto;
+}
+</style>
